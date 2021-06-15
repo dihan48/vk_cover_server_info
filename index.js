af0a6059a9febba62f32ad094cb98bb0a9d598bc
Terrano <42943684+TerranoNissan@users.noreply.github.com>
2021-06-15 19:31:32 +0300

    dx

--- index.js
+++ index.js
@@ -33,12 +33,8 @@ function loop() {
         unirest.get("https://api.battlemetrics.com/servers/2481126")
         .end(function (result) {
             let json = JSON.parse(JSON.stringify(result.body));
-            if(result.status != 200) {
-                logs.server = "off"
-            } else {
-                logs.server = [json.data.attributes.players,json.data.attributes.maxPlayers];
-                createImage(json.data.attributes)
-            }
+            logs.server = [json.data.attributes.players,json.data.attributes.maxPlayers];
+            createImage(json.data.attributes)
         });
 
         function createImage(info) {
